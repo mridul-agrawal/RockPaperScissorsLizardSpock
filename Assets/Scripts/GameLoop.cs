bc93bc8ae9ca008a57832c272bae44d823985fb9
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-02-24 14:59:23 +0530

    Set up the Game Scene.

--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -1,11 +1,36 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameLoop : MonoBehaviour
 {
+    [SerializeField]
+    private UIManager uiManager;
+    private int score;
+    private int round;
+    [SerializeField]
+    private Image cpuSprite;
+    public List<Sprite> objectSprites;
+
+    private void Start()
+    {
+        score = 0;
+        round = 0;
+        StartNewRound();
+    }
+
     private void StartNewRound()
     {
+        // Select a random choice for cpu.
+        ObjectType cpuObject = (ObjectType)UnityEngine.Random.Range(1, Enum.GetValues(typeof(ObjectType)).Length);
 
+        // Update Score and Round UI.
+        uiManager.UpdateScoreUI(score);
+        uiManager.UpdateRoudnUI(round);
+
+        // Update Sprite according to CPU choice.
+        cpuSprite.sprite = objectSprites[(int)cpuObject];
+        cpuSprite.preserveAspect = true;
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+    [SerializeField]
+    private TextMeshProUGUI roundText;
+
+    internal void UpdateScoreUI(int score)
+    {
+        scoreText.text = "SCORE: " + score.ToString();
+    }
+
+    internal void UpdateRoudnUI(int round)
+    {
+        roundText.text = "ROUND: " + round.ToString();
+    }
+}
