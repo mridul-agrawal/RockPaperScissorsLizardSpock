8b096d98ea40101704848ae548ff83e9196a10ba
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-02-24 12:26:43 +0530

    Added UI Elements and LobbyManager for MainMenu.

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button playButton;
+
+    void Start()
+    {
+        playButton.onClick.AddListener(OnClickPlayButton);
+    }
+
+    private void OnClickPlayButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
